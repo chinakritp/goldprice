2ed64c0b04891be2e39cdb313c004901ff819208
chinakritp <chinakrit.p@gmail.com>
2022-01-01 23:35:36 +0700

    update v2

--- script.js
+++ script.js
@@ -5,13 +5,7 @@ jQuery(document).ready(function () {
     shuffle: true,
     transitionDuration: 2000,
     animation: "kenburns",
-    slides: [
-      { src: "img/home-bg.jpg" },
-      { src: "img/01.jpg" },
-      { src: "img/02.jpg" },
-      { src: "img/03.jpg" },
-      { src: "img/04.jpg" },
-      { src: "img/05.jpg" },
+    slides: [{ src: "img/01.jpg" },{ src: "img/02.jpg" },{ src: "img/03.jpg" },{ src: "img/04.jpg" },{ src: "img/05.jpg" },{ src: "img/06.jpg" },{ src: "img/07.jpg" },{ src: "img/08.jpg" },{ src: "img/09.jpg" },{ src: "img/10.jpg" },{ src: "img/11.jpg" },{ src: "img/12.jpg" },{ src: "img/13.jpg" },{ src: "img/14.jpg" },{ src: "img/15.jpg" },{ src: "img/16.jpg" },{ src: "img/17.jpg" },{ src: "img/18.jpg" },{ src: "img/19.jpg" },{ src: "img/20.jpg" },{ src: "img/21.jpg" },{ src: "img/22.jpg" },{ src: "img/23.jpg" },{ src: "img/24.jpg" },{ src: "img/25.jpg" },{ src: "img/26.jpg" },{ src: "img/27.jpg" },{ src: "img/28.jpg" },{ src: "img/29.jpg" },{ src: "img/30.jpg" },{ src: "img/31.jpg" },{ src: "img/32.jpg" },{ src: "img/33.jpg" },{ src: "img/34.jpg" },{ src: "img/35.jpg" },{ src: "img/36.jpg" },{ src: "img/37.jpg" },{ src: "img/38.jpg" },{ src: "img/39.jpg" },{ src: "img/40.jpg" },{ src: "img/41.jpg" },{ src: "img/42.jpg" },{ src: "img/43.jpg" },{ src: "img/44.jpg" },{ src: "img/45.jpg" },{ src: "img/46.jpg" },{ src: "img/47.jpg" },{ src: "img/48.jpg" },{ src: "img/49.jpg" },{ src: "img/50.jpg" },{ src: "img/51.jpg" },{ src: "img/52.jpg" },{ src: "img/53.jpg" },{ src: "img/54.jpg" },{ src: "img/55.jpg" },{ src: "img/56.jpg" },{ src: "img/57.jpg" },{ src: "img/58.jpg" },{ src: "img/59.jpg" },{ src: "img/60.jpg" },{ src: "img/61.jpg" },{ src: "img/62.jpg" },{ src: "img/63.jpg" },{ src: "img/64.jpg" },{ src: "img/65.jpg" },{ src: "img/66.jpg" },{ src: "img/67.jpg" },{ src: "img/68.jpg" },{ src: "img/69.jpg" },
     ],
   });
 });
